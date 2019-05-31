c9b5169c6f36613bbd01fe1bcb692df255721202
Brian Hensley <brnhensley@gmail.com>
2019-05-31 14:11:44 -0700

    add comments and organize tests

--- spec/galc-spec.js
+++ spec/galc-spec.js
@@ -1,5 +1,7 @@
 import { Galc } from './../src/galc.js';
 
+  // General methods
+
 describe('Galc', function() {
   it('should create a Galc object', function ()
   {
@@ -30,32 +32,13 @@ describe('Galc', function() {
     expect(overTime).toEqual(47.5)
   });
 
+  // Mercury Tests
+
   it('should convert earth years to Mercury years', function ()
   {
     let dob = new Galc(new Date(1984, 3, 13));
     let mercAge = dob.getMercuryYears();
     expect(mercAge).toEqual(145.8)
-  });
-
-  it('should convert earth years to Venus years', function ()
-  {
-    let dob = new Galc(new Date(1984, 3, 13));
-    let venusAge = dob.getVenusYears();
-    expect(venusAge).toEqual(56.5)
-  });
-
-  it('should convert earth years to Mars years', function ()
-  {
-    let dob = new Galc(new Date(1984, 3, 13));
-    let marsAge = dob.getMarsYears();
-    expect(marsAge).toEqual(18.6)
-  });
-
-  it('should convert earth years to Jupiter years', function ()
-  {
-    let dob = new Galc(new Date(1984, 3, 13));
-    let jupiterAge = dob.getJupiterYears();
-    expect(jupiterAge).toEqual(2.95)
   });
 
   it('should convert earth years left to Mercury years left', function ()
@@ -65,11 +48,43 @@ describe('Galc', function() {
     expect(mercYearsLeft).toEqual(152.1)
   });
 
+  it('should convert the number of earth years over life expectancy to Mercury years', function ()
+  {
+    let dob = new Galc(new Date(1947, 1, 1));
+    let mercYearsLeft = dob.getMercuryYearsLeft();
+    expect(mercYearsLeft).toEqual("You are 2.1 Mercurial years over expected!")
+  });
+
+  // Venus Tests
+
+  it('should convert earth years to Venus years', function ()
+  {
+    let dob = new Galc(new Date(1984, 3, 13));
+    let venusAge = dob.getVenusYears();
+    expect(venusAge).toEqual(56.5)
+  });
+
   it('should convert earth years left to Venus years left', function ()
   {
     let dob = new Galc(new Date(1984, 3, 13));
     let venusYearsLeft = dob.getVenusYearsLeft();
     expect(venusYearsLeft).toEqual(58.9)
+  });
+
+  it('should convert the number of earth years over life expectancy to Venus years', function ()
+  {
+    let dob = new Galc(new Date(1947, 1, 1));
+    let venYearsLeft = dob.getVenusYearsLeft();
+    expect(venYearsLeft).toEqual("You are 0.8 Venusian years over expected!")
+  });
+
+  // Mars Tests
+
+  it('should convert earth years to Mars years', function ()
+  {
+    let dob = new Galc(new Date(1984, 3, 13));
+    let marsAge = dob.getMarsYears();
+    expect(marsAge).toEqual(18.6)
   });
 
   it('should convert earth years left to Mars years left', function ()
@@ -79,25 +94,20 @@ describe('Galc', function() {
     expect(marsYearsLeft).toEqual(19.4)
   });
 
+  // Jupiter Tests
+
+  it('should convert earth years to Jupiter years', function ()
+  {
+    let dob = new Galc(new Date(1984, 3, 13));
+    let jupiterAge = dob.getJupiterYears();
+    expect(jupiterAge).toEqual(2.95)
+  });
+
   it('should convert earth years left to Jupiter years left', function ()
   {
     let dob = new Galc(new Date(1984, 3, 13));
     let jupYearsLeft = dob.getJupiterYearsLeft();
     expect(jupYearsLeft).toEqual(3.08)
-  });
-
-  it('should convert the number of earth years over life expectancy to Mercury years', function ()
-  {
-    let dob = new Galc(new Date(1947, 1, 1));
-    let mercYearsLeft = dob.getMercuryYearsLeft();
-    expect(mercYearsLeft).toEqual("You are 2.1 Mercurial years over expected!")
-  });
-
-  it('should convert the number of earth years over life expectancy to Venus years', function ()
-  {
-    let dob = new Galc(new Date(1947, 1, 1));
-    let venYearsLeft = dob.getVenusYearsLeft();
-    expect(venYearsLeft).toEqual("You are 0.8 Venusian years over expected!")
   });
 
 });
